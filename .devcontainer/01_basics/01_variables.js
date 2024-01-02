37223288a255bd7eac3cdefa4bee4192e5db8f94
Rathoreprerna02 <123246504+Rathoreprerna02@users.noreply.github.com>
2024-01-02 08:44:40 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 030922
+let accountEmail = "prerna@google.com"
+var accountPassword ="12345"
+accountCity ="Malegaon"
+
+// accountId = 2 not allowed
+
+accountEmail ="aws@aws.com"
+accountPassword ="112233"
+accountCity ="Nandgaon"
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity])
