2ece1b332bdb72c8e59f994e94cd447c5c48c73a
Rathoreprerna02 <123246504+Rathoreprerna02@users.noreply.github.com>
2024-01-02 09:19:45 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3) // we are using nodejs, not browser
+
+console.log( 3 + 3)
+
+// code readability should be high
+
+console.log("Happy New Year")
+
+
+let name ="Merida"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalonr value
+// undefined => 
+// symbol => used for uniqueness
+
+
+// object
+
+console.log(typeof undefined); // output - undefined
+console.log(typeof null); // output - object
