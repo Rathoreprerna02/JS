e5741f94214946b13a18c461ba25d8d6255440be
Rathoreprerna02 <123246504+Rathoreprerna02@users.noreply.github.com>
2024-01-16 09:17:36 +0000

    while loop

---
+++ 01_basics/01_basics /05_iterations/two.js
@@ -0,0 +1,26 @@
+// while loop only condition get checked
+
+
+let index = 0
+while (index <= 10) {
+    //console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["merida", "forzen", "cindrella"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score <= 10);
